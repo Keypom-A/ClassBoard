a8207fac7a0765a03cca964e2f2eb51d3a5048bb
Keypom-A <halkey.pom@gmail.com>
2026-05-11 19:10:45 +0900

    h2.py

--- h2.py
+++ h2.py
@@ -948,5 +948,8 @@ def handle_chat_message(data):
 
 
 if __name__ == '__main__':
+    import eventlet
+    eventlet.monkey_patch()
+
     port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host='0.0.0.0', port=port)
